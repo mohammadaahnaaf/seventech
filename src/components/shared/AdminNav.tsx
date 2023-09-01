89512fd2f2a97a4480f3cf06e52f4be1664a790c
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-09-01 21:02:53 +0600

    feat: :zap: final commit

--- src/components/shared/AdminNav.tsx
+++ src/components/shared/AdminNav.tsx
@@ -121,10 +121,10 @@ export const AdminNav = (props: Props) => {
 
                                         {!useri && (
                                             <div className='flex justify-between gap-2 ml-3'>
-                                                <Link href='/signin' className='bg-red-600 hover:bg-white text-white ring-0 focus:ring-2 ring-white hover:ring-red-600 hover:text-black py-1 px-3 rounded-md'>
+                                                <Link href='/signin' className='bg-sky-600 hover:bg-white text-white ring-0 focus:ring-1 ring-white hover:ring-sky-600 hover:text-black py-1 px-3 rounded-sm'>
                                                     Signup
                                                 </Link>
-                                                <Link href='/login' className='bg-white hover:bg-red-600 ring-0 focus:ring-2 ring-red-600 hover:ring-white hover:text-white py-1 px-3 rounded-md'>
+                                                <Link href='/login' className='bg-white hover:bg-sky-600 ring-0 focus:ring-1 ring-sky-600 hover:ring-white hover:text-white py-1 px-3 rounded-sm'>
                                                     Login
                                                 </Link>
                                             </div>
@@ -135,7 +135,7 @@ export const AdminNav = (props: Props) => {
                                         <Menu as="div" className="ml-3 relative">
                                             <div>
                                                 {useri && (
-                                                    <Menu.Button className="text-sky-600 hover:bg-red-600 hover:bg-opacity-25 bg-sky-600 focus:bg-opacity-25 bg-opacity-30 flex p-[8px] rounded-full relative hover:text-red-600">
+                                                    <Menu.Button className="text-sky-600 hover:bg-gray-600 hover:bg-opacity-25 bg-sky-600 focus:bg-opacity-25 bg-opacity-30 flex p-[8px] rounded-full relative hover:text-red-600">
                                                         <span className="sr-only">Open user menu</span>
                                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
                                                             <path fillRule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
